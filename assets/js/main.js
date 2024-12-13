9825d5291efcaf14c2c4d9c7818d05a4ceb0fbe8
pravin-upfinzo <praveenkumar@upfinzo.in>
2024-12-13 15:31:13 +0530

    Add files via upload
    updated js

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ gsap.fromTo(".inner-text .dd", {
 	}
 });
 
-
+// Aboutus
 TweenLite.fromTo(".fz_core_text img", {
     opacity: 0.5,
     scale: 0.7,
@@ -77,7 +77,7 @@ TweenLite.fromTo(".fz_core_text img", {
         scrub: 1,
     }
 });
-
+// Product
 let sections = gsap.utils.toArray(".fz_product_panel");
 gsap.to(sections, {
 	xPercent: -100 * (sections.length - 1),
@@ -97,7 +97,7 @@ gsap.to(sections, {
 /* Feature */
 let sm = gsap.matchMedia();
 
-sm.add("(min-width: 1130px)", () => {
+sm.add("(min-width: 1200px)", () => {
   gsap.registerPlugin(ScrollToPlugin);
 
   /* CARD ANIMATION */
@@ -126,8 +126,31 @@ sm.add("(min-width: 1130px)", () => {
       end: `'+=${endTime}px'`,
       pinSpacing: true,
       scrub: 0.2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-      markers: false
+      markers: false,
+	  onUpdate: (self) => {
+        const progress = self.progress; // progress value between 0 and 1
+        const activeIndex = Math.floor(progress * images.length); // determine active index
+		images.forEach((image, index) => {
+			if (index === activeIndex) {
+			  image.classList.add("active");
+			} else {
+			  image.classList.remove("active");
+			}
+		  });
+  
+		  listItems.forEach((item, index) => {
+			if (index === activeIndex) {
+			  item.classList.add("active");
+			} else {
+			  item.classList.remove("active");
+			}
+		  });
+		}
     }
+  });
+// Example animations for cards
+images.forEach((image, index) => {
+    //tl.to(image, { y: -50, opacity: 1, duration: 0.5 }, index * 0.5);
   });
 
   /* FOR LARGE DEVICE */
@@ -246,63 +269,74 @@ sm.add("(min-width: 1130px)", () => {
   });
 });
 
-
-// Page loader
-$(function() {
-	function animationStart() {
-		$('#container').addClass('fin');
-	}
-	function resetAnimation() {
-		$('#container').removeClass('fin');
-		setTimeout(animationStart, 50); // Short delay to allow CSS to reset
-	}
-	// Start the animation immediately
-	animationStart();
-	// Total duration of the animation
-	const animationDuration = 7500; // 7.5 seconds
-	// Use setInterval to reset the animation after it finishes
-	setInterval(resetAnimation, animationDuration);
-});
-window.addEventListener('load', function () {
-	setTimeout(function () {
-		// Hide the loader and show the content
-		document.getElementById('loader').style.display = 'none';
-		document.getElementById('content').style.display = 'block';
-	}, 3000); // Wait for 3 seconds before showing content
-});
 // Counter
-var counted = 0;
-$(window).scroll(function() {
+// var counted = 0;
+// $(window).scroll(function() {
 
-	var oTop = $('#counter').offset().top - window.innerHeight;
-	if (counted == 0 && $(window).scrollTop() > oTop) {
-		$('.count').each(function() {
-		var $this = $(this),
-			countTo = $this.attr('data-count');
-		$({
-			countNum: $this.text()
-		}).animate({
-			countNum: countTo
-			},
+// 	var oTop = $('#counter').offset().top - window.innerHeight;
+// 	if (counted == 0 && $(window).scrollTop() > oTop) {
+// 		$('.count').each(function() {
+// 		var $this = $(this),
+// 			countTo = $this.attr('data-count');
+// 		$({
+// 			countNum: $this.text()
+// 		}).animate({
+// 			countNum: countTo
+// 			},
 
-			{
+// 			{
 
-			duration: 2000,
-			easing: 'swing',
-			step: function() {
-				$this.text(Math.floor(this.countNum));
-			},
-			complete: function() {
-				$this.text(this.countNum);
-				//alert('finished');
-			}
+// 			duration: 2000,
+// 			easing: 'swing',
+// 			step: function() {
+// 				$this.text(Math.floor(this.countNum));
+// 			},
+// 			complete: function() {
+// 				$this.text(this.countNum);
+// 				//alert('finished');
+// 			}
 
-			});
-		});
-		counted = 1;
+// 			});
+// 		});
+// 		counted = 1;
+// 	}
+
+// });
+
+function animateCounter(counter) {
+	const targetValue = parseInt(counter.dataset.value, 10);
+	let current = 0;
+	const increment = Math.ceil(targetValue / 100);
+	const duration = 2000; // Duration in ms
+	const interval = 10;
+
+	// Reset the counter to 0 before animating
+	counter.textContent = '0';
+
+	function updateCount() {
+	  current += increment;
+	  if (current > targetValue) current = targetValue;
+	  counter.textContent = current.toLocaleString();
+	  if (current < targetValue) {
+		requestAnimationFrame(updateCount);
+	  }
 	}
+	updateCount();
+  }
 
-});
+  // Intersection Observer to trigger animation on visibility
+  const observer = new IntersectionObserver((entries) => {
+	entries.forEach(entry => {
+	  if (entry.isIntersecting) {
+		document.querySelectorAll('.js-count-up').forEach(counter => {
+		  animateCounter(counter);
+		});
+	  }
+	});
+  }, { threshold: 0.5 }); // Trigger when 50% of section is visible
+
+  // Observe the target section
+  observer.observe(document.querySelector('#counter-section'));
 
 // Testimonial
 const testimonial_items = document.querySelectorAll(".fz_testimonial_item")
@@ -325,11 +359,30 @@ testimonial_items.forEach(function (item, index) {
   }
 })
 
-window.addEventListener("click", () => {
-  clearActiveImage()
-})
+// window.addEventListener("click", () => {
+//   clearActiveImage()
+// })
 
+// Audio
+document.addEventListener("DOMContentLoaded", function () {
+    const audio = document.getElementById("sectionAudio");
+    const targetSection = document.getElementById("targetSection");
+    // Create an Intersection Observer
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                audio.play().catch(error => {
+                    console.log("Audio playback was blocked by the browser:", error);
+                });
+            } else {
+                audio.pause(); // Pause audio when the section is out of view
+            }
+        });
+    });
 
+    // Observe the target section
+    observer.observe(targetSection);
+});
 
 
       // Banner script
@@ -417,12 +470,12 @@ function loop() {
 
 	// Clear and render background
 	CTX.clearRect(0, 0, W, H);
-	const backgroundImage = new Image();
-	backgroundImage.src = "image/Dotted.svg";
-	backgroundImage.onload = () => {
-		CTX.globalAlpha = 1;
-		CTX.drawImage(backgroundImage, 0, 0, W, H);
-	};
+	//const backgroundImage = new Image();
+	// backgroundImage.src = "image/Dotted.svg";
+	// backgroundImage.onload = () => {
+	// 	CTX.globalAlpha = 1;
+	// 	CTX.drawImage(backgroundImage, 0, 0, W, H);
+	// };
 
 	// Semi-transparent overlay for sparkle effect
 	// CTX.fillStyle = "rgba(0, 0, 0, 0.15)";
@@ -452,173 +505,5 @@ function init() {
 	loop();
 }
 init();
-
-
-// globe 
-
-class World {
-	renderer;
-	scene;
-	camera;
-	molecule;
-
-	constructor() {
-		this.build();
-
-		window.addEventListener("resize", this.resize.bind(this));
-
-		this.animate = this.animate.bind(this);
-		this.animate();
-	}
-
-	build() {
-		this.scene = new THREE.Scene();
-		this.camera = new THREE.PerspectiveCamera(
-			75,
-			window.innerWidth / window.innerHeight,
-			0.1,
-			1000
-		);
-		this.camera.position.z = 3;
-
-		this.renderer = new THREE.WebGLRenderer({
-			alpha: true, // Ensures transparency
-			antialias: true,
-		});
-		this.renderer.setPixelRatio(window.devicePixelRatio);
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-		// Attach Three.js canvas to a specific container
-		const globeContainer = document.querySelector(".globe-container");
-		globeContainer.appendChild(this.renderer.domElement);
-
-		this.molecule = new Molecule();
-		this.scene.add(this.molecule);
-	}
-
-
-	resize() {
-		const w = window.innerWidth;
-		const h = window.innerHeight;
-		this.camera.aspect = w / h;
-		this.camera.updateProjectionMatrix();
-		this.renderer.setSize(w, h);
-	}
-
-	animate() {
-		requestAnimationFrame(this.animate);
-
-		const time = performance.now() * 0.001;
-
-		this.molecule.animate(time);
-
-		this.renderer.render(this.scene, this.camera);
-	}
-}
-
-class Molecule extends THREE.Object3D {
-	material;
-	geometry;
-	mesh;
-	radius = 1.3;
-	detail = 40;
-	particleSizeMin = 0.01;
-	particleSizeMax = 0.08;
-
-	constructor() {
-		super();
-
-		this.build();
-	}
-
-	build() {
-		this.dot();
-
-		this.geometry = new THREE.IcosahedronBufferGeometry(1, this.detail);
-
-		this.material = new THREE.PointsMaterial({
-			map: this.dot(),
-			blending: THREE.AdditiveBlending,
-			color: 0x67CBAF,
-			depthTest: false
-		});
-
-		this.setupShader(this.material);
-
-		this.mesh = new THREE.Points(this.geometry, this.material);
-		this.add(this.mesh);
-	}
-
-	dot(size = 32, color = "#3EAB90") {
-		const sizeH = size * 0.5;
-
-		const canvas = document.createElement("canvas");
-		canvas.width = canvas.height = size;
-
-		const ctx = canvas.getContext("2d");
-
-		const circle = new Path2D();
-		circle.arc(sizeH, sizeH, sizeH, 0, 2 * Math.PI);
-
-		ctx.fillStyle = color;
-		ctx.fill(circle);
-
-		// debug canvas
-		// canvas.style.position = "fixed"
-		// canvas.style.top = 0
-		// canvas.style.left = 0
-		// document.body.appendChild(canvas)
-
-		return new THREE.CanvasTexture(canvas);
-	}
-
-	setupShader(material) {
-		material.onBeforeCompile = (shader) => {
-			shader.uniforms.time = { value: 0 };
-			shader.uniforms.radius = { value: this.radius };
-			shader.uniforms.particleSizeMin = { value: this.particleSizeMin };
-			shader.uniforms.particleSizeMax = { value: this.particleSizeMax };
-			shader.vertexShader =
-				"uniform float particleSizeMax;\n" + shader.vertexShader;
-			shader.vertexShader =
-				"uniform float particleSizeMin;\n" + shader.vertexShader;
-			shader.vertexShader = "uniform float radius;\n" + shader.vertexShader;
-			shader.vertexShader = "uniform float time;\n" + shader.vertexShader;
-			shader.vertexShader =
-				document.getElementById("webgl-noise").textContent +
-				"\n" +
-				shader.vertexShader;
-			shader.vertexShader = shader.vertexShader.replace(
-				"#include <begin_vertex>",
-				`
-			vec3 p = position;
-			float n = snoise( vec3( p.x*.6 + time*0.2, p.y*0.4 + time*0.3, p.z*.2 + time*0.2) );
-			p += n *0.4;
-  
-			// constrain to sphere radius
-			float l = radius / length(p);
-			p *= l;
-			float s = mix(particleSizeMin, particleSizeMax, n);
-			vec3 transformed = vec3( p.x, p.y, p.z );
-		  `
-			);
-			shader.vertexShader = shader.vertexShader.replace(
-				"gl_PointSize = size;",
-				"gl_PointSize = s;"
-			);
-
-			material.userData.shader = shader;
-		};
-	}
-
-	animate(time) {
-		this.mesh.rotation.set(0, time * 0.2, 0);
-		if (this.material.userData.shader)
-			this.material.userData.shader.uniforms.time.value = time;
-	}
-}
-
-new World();
-
 
 
