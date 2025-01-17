3fd244aea778c0b76951f1e933d7e52c6e437b5b
ragu-upfinzo <ragupathy@upfinzo.in>
2025-01-17 16:47:53 +0530

    blog section images changed and mobile section gif added

--- assets/js/main.js
+++ assets/js/main.js
@@ -797,13 +797,6 @@ document.addEventListener("DOMContentLoaded", function () {
     path: './assets/json/api-dev.json' // Path to the Lottie JSON file
   });
   lottie.loadAnimation({
-    container: document.getElementById('mob-app-animate'), // HTML container for the animation
-    renderer: 'svg',      // Render as SVG
-    loop: true,           // Animation will loop continuously
-    autoplay: true,       // Animation will play automatically
-    path: './assets/json/mob-app-animate.json' // Path to the Lottie JSON file
-  });
-  lottie.loadAnimation({
     container: document.getElementById('faq-animate'), // HTML container for the animation
     renderer: 'svg',      // Render as SVG
     loop: true,           // Animation will loop continuously
