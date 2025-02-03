88cb193a1a36c835b4c75b315c4052b3fc98c260
Madhubala -upfinzo <madhubala@upfinzo.in>
2025-02-03 17:41:44 +0530

    nav linkadded in products

--- partials/footer.php
+++ partials/footer.php
@@ -43,10 +43,10 @@
                 <h4>PRODUCTS</h4>
                 <ul>
                   <li><a href="finbankz.php">FinbankZ</a></li>
-                  <li><a href="#">UPIZor</a></li>
-                  <li><a href="#">FinzOpZ</a></li>
-                  <li><a href="#">FinZro</a></li>
-                  <li><a href="#">FinlyticZ</a></li>
+                  <li><a href="upizor.php">UPIZor</a></li>
+                  <li><a href="finzopz.php">FinzOpZ</a></li>
+                  <li><a href="finzro.php">FinZro</a></li>
+                  <li><a href="finlyticz.php">FinlyticZ</a></li>
                 </ul>
               </div>
               <div class="links-group">
